77ade34e7ea7acb79eaf5f58e97c38e6221091f6
adem kalfalı <ademkalfa9@gmail.com>
2022-12-24 12:31:37 +0300

    models_relation

--- SESSIONS-3/relation/admin.py
+++ SESSIONS-3/relation/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from .models import (
     Profile,
     Address,
-    Product,
+    Product
 )
+
+# Register your models here.
+
 
 admin.site.register(Profile)
 admin.site.register(Address)
 admin.site.register(Product)
-# Register your models here.
--- SESSIONS-3/relation/models.py
+++ SESSIONS-3/relation/models.py
@@ -1,17 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 # Create your models here.
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
     bio = models.TextField(blank=True)
-    image = models.ImageField(upload_to='profile', blank=True, null='True')
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    image = models.ImageField(upload_to='profile', blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user
+        return self.user.username
+    
+    
 
-    '''
+'''
 on_delete properties:
     # CASCADE -> if primary deleted, delete foreing too.
     # SET_NULL -> if primary deleted, set foreign to NULL. (null=True)
@@ -19,20 +22,19 @@ on_delete properties:
     # DO_NOTHING -> if primary deleted, do nothing.
     # PROTECT -> if foreign is exist, can not delete primary.
 '''
-
 class Address(models.Model):
-    name= models.CharField(max_length=20)
-    Address= models.CharField(max_length=150)
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=150)
     city = models.CharField(max_length=20)
     phone = models.CharField(max_length=20)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f' {self.name} {self.user.username}'
+
+class Product(models.Model):
+    name= models.CharField(max_length=100)
+    user = models.ManyToManyField(User)
 
     def __str__(self):
         return self.name
-
-class Product(models.Model):
-    brand = models.CharField(max_length=100)
-    user = models.ManyToManyField()
-
-    def __str__(self):
-        return self.user
