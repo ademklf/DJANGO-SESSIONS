341cfd329bcd15eac201a8bbaad50059767bcbe8
adem kalfalı <ademkalfa9@gmail.com>
2023-01-04 14:23:45 +0300

    user.app

--- SESSIONS-9/user/serializers.py
+++ SESSIONS-9/user/serializers.py
@@ -7,10 +7,11 @@ class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True)
     password2 = serializers.CharField(write_only=True, required=True)
+    first_name = serializers.CharField(required=True)
 
     class Meta:
-        models = User
-        fields= (
+        model = User
+        fields = (
             'id',
             'username',
             'email',
@@ -19,3 +20,22 @@ class RegisterSerializer(serializers.ModelSerializer):
             'password',
             'password2'
         )
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError(
+                {'password': 'Password fields didnt match.'}
+            )
+        return data
+
+    def create(self, validated_data):
+        # password2 kullanılmayacağı için dictten çıkardık
+        validated_data.pop('password2')
+        # password u daha sonra set etmek için değişkene atadık.
+        password = validated_data.pop('password')
+        # username=validate_data['username], email = va.......
+        user = User.objects.create(**validated_data)
+        # password ün encrypte olarak db ye kaydedilmesiniş sağlıyor.
+        user.set_password(password)
+        user.save()
+        return user
